f23ce4a7e581c7d6c5a3e4d85bca0a5de77f736b
iTzFabregas <fabricio-sampaio@usp.br>
2023-09-04 17:25:38 -0300

    create a defines file

---
+++ src/defines.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <thread>
+#include <vector>
+#include <atomic>
+
+#define MAX_CONNECTIONS 50
+
+#define PORT 12345
+
